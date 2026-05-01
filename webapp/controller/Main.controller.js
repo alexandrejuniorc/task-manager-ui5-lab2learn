7f3a4dcb75160f218641a3c47eab268f54ab6f94
Alexandre Junior <alexandre.junior@lab2dev.com>
2026-05-01 22:23:30 +0000

    feat: add odata model

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,38 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/m/MessageToast"],
+    (Controller, JSONModel, MessageToast) => {
+        "use strict";
 
-    return Controller.extend("studies.taskmanager.controller.Main", {
-        onInit() {
-        }
-    });
-});
+        return Controller.extend("studies.taskmanager.controller.Main", {
+            onInit() {
+                const oData = {
+                    tasks: [
+                        {
+                            title: "Estudar live do Start SAP",
+                            description: "Finalizar aula",
+                            status: "FEITO",
+                            priority: "ALTA"
+                        },
+                        {
+                            title: "Revisar conteúdos",
+                            description: "Finalizar próximas aulas",
+                            status: "EM PROGRESSO",
+                            priority: "MEDIA"
+                        },
+                        {
+                            title: "Criar tela inicial",
+                            description: "Montar interface Fiori do app",
+                            status: "NÃO INICIADO",
+                            priority: "BAIXA"
+                        }
+                    ]
+                };
+                const oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+            },
+            onAddTask() {
+                const oModel = this.getView().getModel();
+            }
+        });
+    }
+);
